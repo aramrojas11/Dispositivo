171b61724f0a21aa3af8e40419253fcdaccc41ec
aramrojas11 <114456919+aramrojas11@users.noreply.github.com>
2023-01-27 10:09:38 -0600

    Celular.js

--- Celular.js
+++ Celular.js
@@ -1,16 +1,14 @@
 class Celular extends Dispositivo{
     Camara;
-    NumeroCeular;
-    Contraseña;
+    #NumeroCeular;
+    #Contraseña;
     MemoraGB;
 
-constructor(Camara,NumeroCelular,Contraseña,MemoriaGB){
-    this.Camara = Camara;
-    this.NumeroCelular = NumerolaCelular;
-    this.Contraseña = Contraseña;
-    this.MemoriaGB = MemoriaGB;
 }
-
+function  MostrarCamara ( Camara ) {
+    volver  [ ] ;
 }
-  var Celular_N = new Celular("108MP","7298840993","hola1234567","120GB");
+function  MostrarNumeroCelular( #NumeroCelular ) {
+    volver [ ] ;
+}
 
